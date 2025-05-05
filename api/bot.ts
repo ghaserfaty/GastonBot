da5c46386e6507d4ae7526250b3f056f89958965
Guillermo Haserfaty <ae_ghaserfaty@contractor.indeed.com>
2025-05-05 15:26:08 -0300

    add report feature

--- api/bot.ts
+++ api/bot.ts
@@ -1,40 +1,36 @@
-import { Telegraf, Context } from 'telegraf';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { Expense } from '../src/types/Expense';
-import { appendExpenseToSheet } from '../src/sheets';
-import dotenv from 'dotenv';
+import { Telegraf, Context } from "telegraf";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Expense } from "../src/types/Expense";
+import { appendExpenseToSheet, getReport } from "../src/sheets";
+import dotenv from "dotenv";
 dotenv.config();
 
-const token: string = process.env.BOT_TOKEN ?? '';
+const token: string = process.env.BOT_TOKEN ?? "";
 console.log("INICIANDO TELEGRAF");
 const bot = new Telegraf(token);
 
 const pendingAmounts = new Map<number, number>(); // chatId -> amount
 const categories = [
-    { label: "🛒 Supermercado", value: "Supermercado" },
-    { label: "🌿 Weed", value: "Weed" },
-    { label: "🍕 Delivery", value: "Delivery" },
-    { label: "🍽️ Comidas afuera", value: "Comidas afuera" },
-    { label: "📺 Ocio", value: "Ocio" },
-    { label: "🍺 Salidas", value: "Salidas" },
-    { label: "🚗 Transporte", value: "Transporte" },
-    { label: "✈️ Viajes", value: "Viajes" },
-    { label: "📆 Gastos fijos", value: "Gastos fijos" },
-    { label: "🏡 Alquiler / Hipoteca", value: "Alquiler / Hipoteca" },
-    { label: "💊 Farmacia", value: "Farmacia" },
-    { label: "🏋️‍♂️ Salud & Gimnasio", value: "Salud & Gimnasio" },
-    { label: "👕 Ropa", value: "Ropa" },
-    { label: "🎁 Regalos & Donaciones", value: "Regalos & Donaciones" },
-    { label: "📚 Educación", value: "Educación" },
-    { label: "💻 Tecnología", value: "Tecnología" },
-    { label: "🧼 Hogar", value: "Hogar" },
-    { label: "🐾 Mascotas", value: "Mascotas" }
-  ];
+  { label: "🛒 Supermercado", value: "Supermercado" },
+  { label: "🌿 Weed", value: "Weed" },
+  { label: "🍕 Delivery", value: "Delivery" },
+  { label: "🍽️ Comidas afuera", value: "Comidas afuera" },
+  { label: "📺 Ocio", value: "Ocio" },
+  { label: "🍺 Salidas", value: "Salidas" },
+  { label: "🚗 Transporte", value: "Transporte" },
+  { label: "💊 Farmacia", value: "Farmacia" },
+  { label: "🏋️‍♂️ Salud & Gimnasio", value: "Salud & Gimnasio" },
+  { label: "👕 Ropa", value: "Ropa" },
+  { label: "🎁 Regalos & Donaciones", value: "Regalos & Donaciones" },
+  { label: "📚 Educación", value: "Educación" },
+  { label: "🧼 Hogar", value: "Hogar" },
+  { label: "🐾 Mascotas", value: "Mascotas" },
+];
 
 // Manejador de mensajes
-bot.on('message', async (ctx) => {
-  console.log('Mensaje recibido:', ctx.message);
-  if (!ctx.message || !('text' in ctx.message)) {
+bot.on("message", async (ctx) => {
+  console.log("Mensaje recibido:", ctx.message);
+  if (!ctx.message || !("text" in ctx.message)) {
     return; // Si no es un mensaje de texto, ignora
   }
   const chatId = ctx.chat.id;
@@ -47,22 +43,31 @@ bot.on('message', async (ctx) => {
   if (!isNaN(amount)) {
     pendingAmounts.set(chatId, amount);
     console.log(`Por mandar opciones de categoría`);
-    await ctx.reply("📂 ¿A qué rubro pertenece este gasto?", {
-      reply_markup: {
-        inline_keyboard: categories.map((cat) => [
-          {
-            text: cat.label,
-            callback_data: cat.value,
-          },
-        ]),
-      },
-    }).catch((error) => {
-      console.error("Error al enviar el mensaje de categorías:", error);
-    });
+    await ctx
+      .reply("📂 ¿A qué rubro pertenece este gasto?", {
+        reply_markup: {
+          inline_keyboard: categories.map((cat) => [
+            {
+              text: cat.label,
+              callback_data: cat.value,
+            },
+          ]),
+        },
+      })
+      .catch((error) => {
+        console.error("Error al enviar el mensaje de categorías:", error);
+      });
     return;
   }
 
   await ctx.reply(`❌ Mandá solo el monto del gasto, como por ejemplo: 3500`);
+});
+
+bot.command("report", async (ctx) => {
+  const chatId = ctx.chat.id;
+
+  const result = await getReport(chatId.toString());
+  ctx.reply(result.toString());
 });
 
 // Manejadores de selección de rubro
@@ -71,7 +76,12 @@ categories.forEach((cat) => {
     const chatId = ctx.callbackQuery?.message?.chat.id;
     const category = cat.value;
 
-    if (chatId && category && (chatId.toString().includes("1338920278") || chatId.toString().includes("1817312721"))) {
+    if (
+      chatId &&
+      category &&
+      (chatId.toString().includes("1338920278") ||
+        chatId.toString().includes("1817312721"))
+    ) {
       const amount = pendingAmounts.get(chatId);
 
       if (amount !== undefined) {
@@ -85,14 +95,18 @@ categories.forEach((cat) => {
         try {
           await appendExpenseToSheet(chatId.toString(), expense);
           await ctx.reply(
-            `✅ Gasto registrado:\n💰 $${amount % 1 === 0 ? amount.toString() : amount.toFixed(2)}\n📂 Rubro: ${category}`
+            `✅ Gasto registrado:\n💰 $${
+              amount % 1 === 0 ? amount.toString() : amount.toFixed(2)
+            }\n📂 Rubro: ${category}`
           );
         } catch (error) {
           console.error("Error al escribir en la hoja de cálculo:", error);
           await ctx.reply("⚠️ Hubo un error al registrar el gasto.");
         }
       } else {
-        await ctx.reply("⚠️ No encontré un monto pendiente. Mandá un número primero.");
+        await ctx.reply(
+          "⚠️ No encontré un monto pendiente. Mandá un número primero."
+        );
       }
     }
 
@@ -102,29 +116,21 @@ categories.forEach((cat) => {
   });
 });
 
-const webhookPath = `/api/bot`;
-
 // Handler de Vercel
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
-    if (req.method === 'POST') {
+    if (req.method === "POST") {
       await bot.handleUpdate(req.body);
       res.status(200).end();
     } else {
       res.status(200).send("Bot funcionando via webhook con Telegraf.");
     }
   } catch (error) {
-    console.error('Error handling update:', error);
-    res.status(500).send('Error handling update');
+    console.error("Error handling update:", error);
+    res.status(500).send("Error handling update");
   }
 }
 
-// Desarrollo local (polling)
-if (process.env.NODE_ENV === 'development') {
-  bot.launch().then(() => console.log('Bot lanzado en modo polling'));
+if (process.env.NODE_ENV === "development") {
+  bot.launch().then(() => console.log("Bot lanzado en modo polling"));
 }
-
-// Configurar webhook una vez luego del deploy
-// bot.telegram.setWebhook(`https://TU_PROYECTO.vercel.app${webhookPath}`)
-//   .then(() => console.log('Webhook configurado:', `https://TU_PROYECTO.vercel.app${webhookPath}`))
-//   .catch(console.error);
--- src/sheets.ts
+++ src/sheets.ts
@@ -1,55 +1,87 @@
-import { GoogleAuth } from 'google-auth-library';
-import { sheets_v4 } from '@googleapis/sheets';
+import { GoogleAuth } from "google-auth-library";
+import { sheets_v4 } from "@googleapis/sheets";
 import { Expense } from "./types/Expense";
-import axios from 'axios';
-
+import axios from "axios";
 
 const SPREADSHEET_ID = "1bY081aIOrp9YUsNniatAWtX_JhMWGArFJuAaELKkjNs";
-const SHEET_NAME = "GastonSheet"
+const SHEET_NAME = "GastonSheet";
+
+export const getReport = async (user: string) => {
+  const sheets = await authenticate();
+
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: SPREADSHEET_ID,
+    range: SHEET_NAME,
+  });
+
+  const rows = res.data.values;
+  if (!rows || rows.length === 0) {
+    return "La hoja está vacía.";
+  }
+
+  const encabezado = rows[0];
+  const datos = rows.slice(1);
+
+  const colUser = encabezado.indexOf("User");
+  const colAmount = encabezado.indexOf("Amount (USD)");
+
+  const total = datos
+    .filter((row) => row[colUser]?.toLowerCase() === user)
+    .reduce((sum, row) => sum + parseFloat(row[colAmount] || 0), 0);
+  return total;
+};
 
 async function authenticate() {
   try {
-    const credentialsJSON = JSON.parse(process.env.GOOGLE_CREDENTIALS || '');
+    const credentialsJSON = JSON.parse(process.env.GOOGLE_CREDENTIALS || "");
 
     const auth = new GoogleAuth({
       credentials: credentialsJSON,
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
 
     const sheets = new sheets_v4.Sheets({ auth: auth });
 
     return sheets; // Retornamos el objeto 'sheets' autenticado para usarlo después
   } catch (error) {
-    console.error('Error al autenticar:', error);
+    console.error("Error al autenticar:", error);
     throw error;
   }
 }
 
 export const appendExpenseToSheet = async (user: string, expense: Expense) => {
-    const sheets = await authenticate();
-    const usdValue = await getBlueDollarRate();
-    let usdAmount = 0
-    if(usdValue){
-      usdAmount = expense.amount / usdValue;
-    }
-  
-    await sheets.spreadsheets.values.append({
-      spreadsheetId: SPREADSHEET_ID,
-      range: `${SHEET_NAME}!A1`,
-      valueInputOption: "RAW",
-      requestBody: {
-        values: [[user, expense.category, expense.timestamp,expense.amount, usdValue, usdAmount]],
-      },
-    });
-  };
-
-
-  async function getBlueDollarRate(): Promise<number | null> {
-    try {
-      const response = await axios.get('https://api.bluelytics.com.ar/v2/latest');
-      return response.data.blue.value_buy; // o value_avg, según prefieras
-    } catch (error) {
-      console.error('Error al obtener la cotización del dólar:', error);
-      return null;
-    }
+  const sheets = await authenticate();
+  const usdValue = await getBlueDollarRate();
+  let usdAmount = 0;
+  if (usdValue) {
+    usdAmount = expense.amount / usdValue;
   }
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${SHEET_NAME}!A1`,
+    valueInputOption: "RAW",
+    requestBody: {
+      values: [
+        [
+          user,
+          expense.category,
+          expense.timestamp,
+          expense.amount,
+          usdValue,
+          usdAmount,
+        ],
+      ],
+    },
+  });
+};
+
+async function getBlueDollarRate(): Promise<number | null> {
+  try {
+    const response = await axios.get("https://api.bluelytics.com.ar/v2/latest");
+    return response.data.blue.value_buy; // o value_avg, según prefieras
+  } catch (error) {
+    console.error("Error al obtener la cotización del dólar:", error);
+    return null;
+  }
+}
